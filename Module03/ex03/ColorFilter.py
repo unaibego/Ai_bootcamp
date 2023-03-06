93c1b240d301f9ebcdc8730060ca4dabf416bc94
Unai Begona <ubegona@c4r6s5.42urduliz.com>
2023-03-06 10:09:45 +0100

    mierda

--- Module03/ex03/ColorFilter.py
+++ Module03/ex03/ColorFilter.py
@@ -1,5 +1,6 @@
 import matplotlib.image as mpimg
 import matplotlib.pyplot
+import numpy as np
 
 def decorator(function):
     def problem(self, lista):
@@ -12,6 +13,22 @@ def decorator(function):
 
 class ColorFilter():
     def invert(self, array):
+        return (1 - array)
+    def to_blue(self, array):
+        array[:, :, 0] = np.zeros(np.shape(array[:, :, 0]))
+        array[:, :, 1] = np.zeros(np.shape(array[:, :, 2]))
+        return (array)
+    def to_green(self, array):
+        array[:, :, 0] = array[:, :, 0] * 0
+        array[:, :, 2] = array[:, :, 2] * 0
+        return (array)
+    def to_red(self, array):
+        # blue_array = self.to_blue(array)
+        # green_array = self.to_green(array)
+        array[:, :, 1] = array[:, :, 1] - array[:, :, 1]
+        array[:, :, 2] = array[:, :, 2] - array[:, :, 2]
+        return (array)
+
         
 #bertan shape-k ez ditu parentesiak atributo bat delako. Hau da, "self.shape" moduan definitu dute numpyko sortzaileak eta ez "def shape"
 class ImageProcesor():
@@ -32,4 +49,7 @@ class ImageProcesor():
 
 img = ImageProcesor()
 array = img.load("Cute_acab.png")
+cf = ColorFilter()
+# array = cf.invert(array)
+array = cf.to_red(array)
 img.display(array)
